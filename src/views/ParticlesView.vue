306cd526928dfd50c42d3c2eae6b8bcff191ea2f
Mclin24 <972665788@qq.cpm>
2025-09-06 08:34:55 +0800

    fix: 修复particles配置的json数据格式错误问题

--- src/views/ParticlesView.vue
+++ src/views/ParticlesView.vue
@@ -1,34 +1,34 @@
 <template>
-    <vue-particles
-        id="tsparticles"
-        :particlesInit="particlesInit"
-        :particlesLoaded="particlesLoaded"
-        :options="particlesConfig"
-    >
-    </vue-particles>
+   <vue-particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :particlesLoaded="particlesLoaded"
+      :options="particlesConfig"
+   >
+   </vue-particles>
 </template>
 <script setup lang="ts">
 import { loadFull } from 'tsparticles'
 import particlesConfig from '@/components/particles/nasa-config.json'
 const particlesInit = async (engine: any) => {
-    await loadFull(engine)
+   await loadFull(engine)
 }
 
 const particlesLoaded = async (container: any) => {
-    console.log('Particles container loaded', container)
+   console.log('Particles container loaded', container)
 }
 </script>
 <style>
 #tsparticles {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-image: radial-gradient(at center top, #c4c6c6, #000016);
-    background-size: cover;
-    background-position: 50% 50%;
-    background-repeat: no-repeat;
-    z-index: -1;
+   position: fixed;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   background-image: radial-gradient(at center top, #c4c6c6, #000016);
+   background-size: cover;
+   background-position: 50% 50%;
+   background-repeat: no-repeat;
+   z-index: -1;
 }
 </style>
