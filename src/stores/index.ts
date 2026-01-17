72830aff6e1f896f17ce68adf1f550579c02176f
Mclin24 <972665788@qq.cpm>
2026-01-17 18:30:23 +0800

    feat: create Pinia store structure

---
+++ src/stores/index.ts
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+
+// Example store - can be expanded later
+export const useAppStore = defineStore('app', {
+  state: () => ({
+    // Add state here as needed
+  }),
+  getters: {
+    // Add getters here as needed
+  },
+  actions: {
+    // Add actions here as needed
+  }
+})
