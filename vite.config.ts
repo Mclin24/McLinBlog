ebe0d4709e46d04a58fe2203d2c186248f0f6644
Mclin24 <972665788@qq.cpm>
2026-01-17 18:15:47 +0800

    feat: initialize Vite project structure

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'McLin24 Blog',
+        short_name: 'McLin24',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.220.1:9090',
+        changeOrigin: true
+      }
+    }
+  }
+})
