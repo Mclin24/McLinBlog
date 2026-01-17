f535151d03d000703382dd90de14f547db247180
Mclin24 <972665788@qq.cpm>
2026-01-17 18:32:10 +0800

    feat: update main.ts for Pinia and Vite

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
-// import './registerServiceWorker'
 import router from './router'
-import store from './store'
-// 引入阿里云图标库
-import '../src/assets/iconfont/iconfont.css'
-import ToolsTip from '@/components/toolsTip.vue' // @ is an alias to /src
-import ImageHover from '@/components/ImageHover.vue' // @ is an alias to /src
+import '@/assets/iconfont/iconfont.css'
+import ToolsTip from '@/components/toolsTip.vue'
+import ImageHover from '@/components/ImageHover.vue'
 import Particles from '@tsparticles/vue3'
 import { loadFull } from 'tsparticles'
+
+const pinia = createPinia()
 const mcApp = createApp(App)
+
 mcApp
-    .use(store)
+    .use(pinia)
     .use(router)
     .use(Particles, {
         init: async (engine: any) => {
-            await loadFull(engine) // you can load the full tsParticles library from "tsparticles" if you need it
+            await loadFull(engine)
         }
     })
     .component('ImageHover', ImageHover)
